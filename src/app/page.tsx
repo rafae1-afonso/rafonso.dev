8ca0fb3bdd7a570771fe7bc610be7a745e368a85
Rafael Afonso <122814044+rafae1-afonso@users.noreply.github.com>
2025-10-15 21:17:00 -0300

    setup commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="tv-tubo-border bg-[url(/tube-border.png)] w-full h-full absolute pointer-events-none z-10">
+
+    </div>
+  );
+}
